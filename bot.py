1dc0f446031548728f2735b2954f4d5d739c63d9
CAN ROLLAS <57571385+canrollas@users.noreply.github.com>
2021-02-08 03:27:16 +0300

    users appended with loop

--- bot.py
+++ bot.py
@@ -16,7 +16,8 @@ In recent years, graph theory has established itself as an important mathematica
 a wide variety of subjects, ranging from operational research and chemistry to genetics
 and linguistics, and from electrical engineering and geography to sociology and architecture.
 At the same time it has also emerged as a worthwhile mathematical discipline
-in its own right.
+in its own right."""
+data2="""
 In view of this, there is a need for an inexpensive introductory text on the subject,
 suitable both for mathematicians taking courses in graph theory and also for nonspecialists
 wishing to learn the subject as quickly as possible. It is my hope that this
@@ -25,38 +26,39 @@ a basic knowledge of elementary set theory and matrix theory, although a further
 knowledge of abstract algebra is needed for more difficult exercises.
 The contents of this book may be conveniently divided into four parts. The first of
 these (Chapters 1-4) provides a basic foundation course, containing definitions and
-examples of graphs, connectedness, Eulerian and Hamiltonian paths and cycles, and
+examples of graphs, """
+data3="""connectedness, Eulerian and Hamiltonian paths and cycles, and
 trees. This is followed by two chapters (Chapters 5 and 6) on planarity and colouring,
 with special reference to the four-colour theorem. The third part (Chapters 7 and 8)
 deals with the theory of directed graphs and with transversal theory, with applications
 to critical path analysis, Markov chains and network flows. The book ends with a chapter
 on matroids (Chapter 9), which ties together material from the previous chapters
-and introduces some recent developments.
-Throughout the book I have attempted to restrict the text to basic material, using
+and introduces some recent developments."""
+data4="""Throughout the book I have attempted to restrict the text to basic material, using
 exercises as a means for introducing less important material. Of the 250 exercises,
 some are routine examples designed to test understanding of the text, while others
 will introduce you to new results and ideas. You should read each exercise,
 whether or not you work through it in detail. Difficult exercises are indicated by an
-asterisk.
-I have used the symbol // to indicate the end of a proof, and bold-face type is used
+asterisk."""
+data5="""I have used the symbol // to indicate the end of a proof, and bold-face type is used
 for definitions. The number of elements in a set S is denoted by LSI, and the empty set
-is denoted by 0.
-A substantial number of changes have been made in this edition. The text has been
+is denoted by 0."""
+data6="""A substantial number of changes have been made in this edition. The text has been
 revised throughout, and some terminology has been changed to fit in with current
 usage. In addition, solutions are given for some of the exercises; these exercises are
 indicated by the symbols next to the exercise number. Several changes have arisen as
 """
-
+lister=[data,data2,data3,data4,data5,data6]
+names=["bilgehanay","canrollas","alindakabadayı","berkcantug","özlemkaba","rollasekrem"]
 
 try:
-
+    
     driver.find_element_by_id("username").send_keys("xxxxxxxxxx@std.iyte.edu.tr")
     driver.find_element_by_id ("password").send_keys("*************")
     driver.find_element_by_class_name("ZLoginButton").click()    
     time.sleep(3)
     counter=0
-    while(True):
-        
+    for i,j in zip(names,lister):
         while True:
           x = chr(ord('a') + random.randint(0, 26))
           if x != "l": break
@@ -66,11 +68,11 @@ try:
 
         driver.find_element_by_id("zb__NEW_MENU").click()
         time.sleep(1)
-        driver.find_element_by_id("zv__COMPOSE-1_to_control").send_keys("bilgehanay@std.iyte.edu.tr")
+        driver.find_element_by_id("zv__COMPOSE-1_to_control").send_keys(i+"@std.iyte.edu.tr")
         driver.find_element_by_id("zv__COMPOSE-1_subject_control").send_keys(x)
-        driver.find_element_by_id("ZmHtmlEditor1_body_ifr").send_keys((data))
+        driver.find_element_by_id("ZmHtmlEditor1_body_ifr").send_keys(j)
         driver.find_element_by_id('zb__COMPOSE-1__SEND_left_icon').click()
-        print("İsmi verilen kisiye {} tane mail gönderildi".format(counter))
+        print("İsmi {} kisiye {} tane mail gönderildi".format(i,counter))
         counter=counter+1
         time.sleep(1)
         
